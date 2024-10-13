7a8ed22ef116f763ec1a750345f91f0df804c9f9
ndelarans <118682151+ndelarans@users.noreply.github.com>
2024-10-12 23:57:34 -0500

    Delete src directory

--- src/user/v1/read.user.action.ts
+++
@@ -1,9 +0,0 @@
-import { UserModel, UserType } from "./user.model";
-
-async function readUserAction(): Promise<UserType[]> {
-  const results = await UserModel.find();
-
-  return results;
-}
-
-export default readUserAction;
